2220e34e8864f9bac3c819cc4744eb353e429e50
Jarjajarr <110670324+Jarjajarr@users.noreply.github.com>
2023-09-18 13:39:07 +0700

    fix ActivityCard

--- src/components/ActivityCard.jsx
+++ src/components/ActivityCard.jsx
@@ -9,22 +9,22 @@ const ActivityCard = () => {
     const rows = Array.from({ length: numRows }, (_, rowIndex) =>
       activityList.slice(rowIndex * 3, rowIndex * 3 + 3)
     );
-  
+  //activity-card-box-in-activitypage
     return (
       <div className=" m-5 ">
         {rows.map((row, rowIndex) => (
-          <div key={rowIndex} className="flex">
+          <div key={rowIndex} className="flex ">
             {row.map((activity, index) => (
-              <div key={index} className="card w-1/3 bg-base-100  drop-shadow image-full">
+              <div key={index} className="card w-1/3 bg-base-100  image-full p-4 ">
                 <figure>
                   <img src={activity.activity} alt={activity.name} />
                 </figure>
-                <div className="card-body">
+                <div className="card-body drop-shadow-xl rounded-2xl activity-card-box-in-activitypage ">
                   <h1>
                     <strong>{activity.name}</strong>
                   </h1>
                   <p>{activity.description}</p>
-                  <div className="card-actions justify-end">
+                  <div className="card-actions justify-end drop-shadow-xl">
                     <button className="btn btn-primary">Do Now</button>
                   </div>
                 </div>
