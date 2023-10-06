befec291a0f85b9c357d97da3f7eb7cb748a6049
peth12 <peth12>
2023-10-06 09:05:43 +0700

    signup page

--- src/views/SignUp/SignUp.jsx
+++ src/views/SignUp/SignUp.jsx
@@ -1,44 +1,120 @@
 import React from "react";
-import "./Signup.css"
+import "./Signup.css";
 import { NavbarLandingPage } from "../../components/NavbarLogin";
 const SignUp = () => {
   return (
     <>
       <NavbarLandingPage />
-      <div className="left flex h-[100vh]">
+      <div className=" flex h-[100vh]">
         {/* section 1 */}
-        <div className="h-[100%] w-[100%]"></div>
+        <div className="left hidden md:block h-[100%] w-[100%]"></div>
         {/* section 2 */}
         <div className="right h-[100%] bg-white w-[100%]">
-          <div className="flex justify-center items-center w-[100%] h-[100%]">
-            <div className="flex-col ">
-              <h1 className="text-primary text-[48px] italic font-bold text-center">
-                Sign up
+          <div className="flex justify-center items-center w-[100%] h-[100%] ">
+            <div className="flex-col">
+              <h1 className=" text-[70px] text-primary  italic font-bold text-center py-3">
+                Sign Up
               </h1>
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text">Email or Username</span>
+                  <span className="label-text text-primary font-semibold text-[16px]">First Name</span>
                 </label>
                 <input
                   type="text"
-                  placeholder="Enter Email or Username"
+                  placeholder="Enter Firstname"
                   className="input input-bordered w-80"
                 />
               </div>
+              <div className="form-control py-3">
+                <label className="label">
+                  <span className="label-text text-primary font-semibold text-[16px]">LastName</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="Enter Lastname"
+                  className="input input-bordered w-80"
+                />
+              </div>
+              <div className="flex justify-between">
+                <div className="form-control py-3">
+                  <label className="label">
+                    <span className="label-text text-primary font-semibold text-[16px]">Date of Birth</span>
+                  </label>
+                  <input
+                    type="date"
+                    placeholder="Enter Lastname"
+                    className="input input-bordered w-40 text-secondary"
+                  />
+                </div>
+
+                <div className="form-control py-3">
+                  <label className="label">
+                    <span className="label-text text-primary font-semibold text-[16px]">
+                      Gender
+                    </span>
+                  </label>
+                  <select className="select select-bordered text-gray-400">
+                    <option disabled selected>
+                      None
+                    </option>
+                    <option>Female</option>
+                    <option>Male</option>
+                    <option>Not specified</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="flex justify-between">
+                <div className="form-control py-3">
+                  <label className="label">
+                    <span className="label-text text-primary font-semibold text-[16px]">Weigth</span>
+                  </label>
+                  <input
+                    type="number"
+                    placeholder="Enter Weigth"
+                    className="input input-bordered w-[9.5rem] text-secondary"
+                  />
+                </div>
+                <div className="form-control py-3">
+                  <label className="label">
+                    <span className="label-text text-primary font-semibold text-[16px]">Height</span>
+                  </label>
+                  <input
+                    type="number"
+                    placeholder="Enter Height"
+                    className="input input-bordered w-[9.5rem] text-secondary"
+                  />
+                </div>
+
+              </div>
+
               <div className="form-control">
                 <label className="label">
-                  <span className="label-text">Password</span>
+                  <span className="label-text text-primary font-semibold text-[16px]">Email</span>
                 </label>
                 <input
                   type="text"
+                  placeholder="Enter Email"
+                  className="input input-bordered w-80"
+                />
+              </div>
+              <div className="form-control py-3">
+                <label className="label">
+                  <span className="label-text text-primary font-semibold text-[16px]">Password</span>
+                </label>
+                <input
+                  type="password"
                   placeholder="Enter password"
                   className="input input-bordered w-80"
                 />
               </div>
-              <p></p>
+
+              {/* <Link to={"/activity"}> */}
               <button className="btn-primary w-80 rounded-md p-1 text-[24px] font-semibold text-white mt-4">
-                Sign up
+                Sign Up
               </button>
+
+              {/* </Link> */}
             </div>
           </div>
         </div>
