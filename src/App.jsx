f23b197493ad68b0cd3d237026e95d64750c025e
peth12 <peth12>
2023-10-08 13:11:40 +0700

    app

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import "./App.css";
+import { Layout } from "./components/Layout";
+import { LandingPage } from "./views/LandingPage";
+function App() {
+  return <>
+  </>;
+}
+
+export default App;
