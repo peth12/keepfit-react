529e9d0d800e5038d5998132c429c237c0a98539
Jarjajarr <110670324+Jarjajarr@users.noreply.github.com>
2023-10-17 09:57:44 +0700

    updated add activityform

--- src/components/ActivityComponent/AddActivityForm.jsx
+++ src/components/ActivityComponent/AddActivityForm.jsx
@@ -4,7 +4,6 @@ import { useData } from "./ActivityData";
 const AddActivityForm = ({ toggleFormVisibility, defaultType }) => {
   const { activityList } = useData();
 
-
   const selectedActivity = activityList.find(
     (activity) => activity.name === defaultType
   );
@@ -37,7 +36,7 @@ const AddActivityForm = ({ toggleFormVisibility, defaultType }) => {
 
       <div className="relative z-10 gradient-background p-8 rounded-lg shadow-lg w-96 h-fit">
         <span
-          className="absolute top-0 right-0 m-2 text-gray-500 cursor-pointer p-2"
+          className="absolute top-0 right-0 m-2 text-white cursor-pointer p-2"
           onClick={toggleFormVisibility}
           style={{ transform: "scale(2.5)" }}
         >
@@ -46,70 +45,75 @@ const AddActivityForm = ({ toggleFormVisibility, defaultType }) => {
 
         <form onSubmit={handleSubmit}>
           <div className="mb-7 flex justify-start items-center gap-3 ">
-            <div className="text-3xl text-white font-bold mb-1 text-center pb-1">{selectedActivity.name}</div>
+            <div className="text-3xl text-white font-bold mb-1 text-center pb-1">
+              {selectedActivity.name}
+            </div>
             <div>{selectedActivity.icon}</div>
           </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-xl font-bold mb-2">
-              Activity Name:
-            </label>
-            <input
-              type="text"
-              name="name"
-              placeholder=" Morning Jogging, Basic Yoga ..."
-              value={activityData.name}
-              onChange={handleInputChange}
-              className="form-input bg-slate-200 w-80 rounded-sm"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-xl font-bold mb-2">
-              Description:
-            </label>
-            <textarea
-              name="description"
-              value={activityData.description}
-              onChange={handleInputChange}
-              placeholder=" Tell about your activity..."
-              className="form-textarea bg-slate-200 w-80 rounded-sm"
-            ></textarea>
+          <div className="mb-4 text-orange-400">
+            <div className="mb-4 ">
+              <label className="block text-xl font-bold mb-2 mt-12">
+                Activity Name:
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder=" Morning Jogging, Basic Yoga ..."
+                value={activityData.name}
+                onChange={handleInputChange}
+                className="input input-bordered  w-80 rounded-sm"
+              />
+            </div>
+            <div className="mb-4">
+              <label className="block  text-xl font-bold mb-2">
+                Description:
+              </label>
+              <input
+                name="description"
+                value={activityData.description}
+                onChange={handleInputChange}
+                placeholder=" Tell about your activity..."
+                className="input input-bordered  w-80 rounded-sm "
+              ></input>
+            </div>
+
+            <div className="mb-4">
+              <label className="block  text-xl font-bold mb-2">
+                Duration (minutes):
+              </label>
+              <input
+                type="text"
+                name="duration"
+                placeholder=" only number here"
+                value={activityData.duration}
+                onChange={handleInputChange}
+                className="input input-bordered  w-80 rounded-sm"
+              />
+            </div>
+            <div className="flex justify-center items-center gap-4">
+              <div className="">
+                <label className="block  text-xl font-bold">Date:</label>
+                <input
+                  type="date"
+                  name="date"
+                  value={activityData.date}
+                  onChange={handleInputChange}
+                  className="input input-bordered  w-fit text-black h-10"
+                />
+              </div>
+              <div className="h-fit mt-7">
+                <label className=" bg-cyan-600  hover-bg-cyan-400 duration-150 text-white font-semibold py-2 px-4 rounded cursor-pointer items-center ">
+                  <input
+                    type="file"
+                    className="file-input file-input-bordered file-input-info w-full max-w-xs h-10"
+                  />{" "}
+                  Upload image
+                </label>
+              </div>
+            </div>
           </div>
 
-          <div className="mb-4">
-            <label className="block text-gray-700 text-xl font-bold mb-2">
-              Duration (minutes):
-            </label>
-            <input
-              type="text"
-              name="duration"
-              placeholder=" only number here"
-              value={activityData.duration}
-              onChange={handleInputChange}
-              className="form-input bg-slate-200 w-80 rounded-sm"
-            />
-          </div>
-          <div className="mb-4">
-            <label className="block text-gray-700 text-xl font-bold mb-2">
-              Date:
-            </label>
-            <input
-              type="date"
-              name="date"
-              value={activityData.date}
-              onChange={handleInputChange}
-              className="form-input"
-            />
-          </div>
-          <div className="mb-4 flex justify-center">
-            <label className=" bg-cyan-600  hover-bg-cyan-400 duration-150 text-white font-semibold py-2 px-4 rounded cursor-pointer items-center ">
-              <input
-                type="file"
-                className="file-input file-input-bordered file-input-info w-full max-w-xs"
-              />{" "}
-              Upload image
-            </label>
-          </div>
-          <div className="text-center">
+          <div className="text-center mt-10">
             <button type="submit" className="btn btn-primary text-white">
               Add Activity
             </button>
