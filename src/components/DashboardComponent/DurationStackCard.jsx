da2e49ab63ddef99cf690693667710c56fdb8696
Jarjajarr <charatvc@gmail.com>
2023-10-22 03:06:18 +0700

    sync data to dashboard

--- src/components/DashboardComponent/DurationStackCard.jsx
+++ src/components/DashboardComponent/DurationStackCard.jsx
@@ -1,16 +1,19 @@
+import { useData } from "./DashboardData";
+
 const DurationStackCard = () => {
+  const {duration} = useData()
   return (
     <div className="flex-col rounded-2xl p-2 ">
       <div className="text-end mt-4 pl-4">
         <h1 className="pt-2 text-start">
-          <div className="text-[50px] font-semibold text-white text-start pe-2 italic">
+          <div className="text-4xl font-semibold text-white text-start pe-2 italic">
             All time summary 
           </div>
         </h1>
         {/* <p className="text-lg bg-white max-w-sm rounded-full  text-black btn normal-case px-7">Week</p> */}
       </div>
       <div className=" text-center">
-        <p className="text-[150px] font-bold text-white italic">277</p>
+        <p className="text-[150px] font-bold text-white italic">{duration.yoga + duration.running + duration.cycling + duration.swimming + duration.boxing}</p>
         <p className="text-[50px] font-semibold text-white text-end pr-4 italic">
           Minutes
         </p>
--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -10,32 +10,34 @@ import {
 import ActivityChart from "../../components/DashboardComponent/ActivityChart";
 import DurationChart from "../../components/DashboardComponent/DurationChart";
 import ActivityChartHorizon from "../../components/DashboardComponent/ActivityChartHorizon";
+import DashboardData from "../../components/DashboardComponent/DashboardData";
 
 export const Dashboard = () => {
   return (
     <Layout>
+      <DashboardData>
       <div className="xl:container mx-auto px-10">
       <div className="flex justify-center mt-24 px-5">
         <div className="mx-auto px-5">
-          <div className="flex flex-col xl:flex-row gap-5 justify-center ">
+          <div className="flex flex-col xl:flex-row gap-5 justify-center mb-20">
             <div className="shadow-xl rounded-xl p-10  gradient-background-user h-auto w-full ">
               <UserCard />
             </div>
-            <div className="shadow-xl rounded-xl  bg-orange-400 h-auto w-full ">
-              <DurationStackCard />
+            <div className="shadow-xl rounded-xl  bg-orange-400 h-auto w-full">
+              <DurationStackCard  />
             </div>
           </div>
           <div className="flex flex-col lg:flex-row gap-5">
             <div className="shadow-xl  rounded-xl  w-full xl:w-4/6 font-bold text-xl h-auto">
               <div className="hidden lg:block p-10">
-              <div className="">Activity Count Overview</div>
+              <div className="mb-10">Activity Count Overview</div>
                 <ActivityChart className="" />
               </div>
               <div className="block lg:hidden">
                 <ActivityChartHorizon className="" />
               </div>
             </div>
-            <div className=" shadow-xl  rounded-xl  w-full lg:w-2/6 font-bold  text-3xl">
+            <div className=" shadow-xl  rounded-xl  w-full lg:w-2/6 font-bold  text-2xl">
               <div className="mt-10 ms-3">Duration Summary Overview</div>
               <div className="p-20">
                 <DurationChart className="" />
@@ -51,9 +53,10 @@ export const Dashboard = () => {
             </button>
           </div>
         </div>
-        
+        {/* <DashboardData/> */}
       </div>
       </div>
+      </DashboardData>
     </Layout>
   );
 };
